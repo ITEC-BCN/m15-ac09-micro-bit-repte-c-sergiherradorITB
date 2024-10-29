9d4387827bac0d5b3a6203c5fbbefb15fdc0cc2b
sergiherradorITB <146918553+sergiherradorITB@users.noreply.github.com>
2024-10-29 13:57:57 +0100

    Radio, enviar cadena y mostrarla

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendString("Hello")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(33)
